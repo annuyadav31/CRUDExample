8d5f0aea22d6989c37d16c2856ba78bd5063cc09
annuyadav31 <annuyadav99539@gmail.com>
2023-04-21 22:04:23 +0100

    Created Interface for Person Service

---
+++ ServiceContracts/Interfaces/IPersonService.cs
@@ -0,0 +1,23 @@
+﻿using ServiceContracts.ModelDTO;
+
+namespace ServiceContracts.Interfaces
+{
+    /// <summary>
+    /// Reprsents business logic for manipulating person entity
+    /// </summary>
+    public interface IPersonService
+    {
+        /// <summary>
+        /// Adds a new person to the existing list of person
+        /// </summary>
+        /// <param name="personAddRequest">object to add</param>
+        /// <returns>Returns generated PersonResponse</returns>
+        PersonResponse AddPerson(PersonAddRequest personAddRequest);
+
+        /// <summary>
+        /// Gets list of all existing persons with PersonResponse Type
+        /// </summary>
+        /// <returns></returns>
+        List<PersonResponse> GetAllPersonsList();
+    }
+}
