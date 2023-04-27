d529f297e60e7fae200d130523db7d4b31f6dc8c
annuyadav31 <annuyadav99539@gmail.com>
2023-04-27 15:10:06 +0100

    Filtered List and Sort List logic

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts.Enums;
 using ServiceContracts.Interfaces;
 using ServiceContracts.ModelDTO;
 using Services;
@@ -17,10 +18,31 @@ namespace CRUDExample.Controllers
 
         [Route("/")]
         [Route("persons/index")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
         {
-           List<PersonResponse> list= _personService.GetAllPersonsList();
-            return View(list); //Views/Persons/Index.html
+            #region "Searching"
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                { nameof(PersonResponse.PersonName),"Person Name" },
+                { nameof(PersonResponse.Email),"Email" },
+                { nameof(PersonResponse.DateOfBirth),"Date of Birth" },
+                { nameof(PersonResponse.Gender),"Gender" },
+                { nameof(PersonResponse.Country),"Country Name" },
+                { nameof(PersonResponse.Address),"Address" },
+            };
+
+            List<PersonResponse> list = _personService.GetFilteredList(searchBy, searchString);
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
+            #endregion
+
+            #region "Sorting"
+            List<PersonResponse> list2=_personService.GetSortedPersons(list,sortBy, sortOrder);
+            ViewBag.CurrentSortBy = sortBy;
+            ViewBag.CurrentSortOrder = sortOrder;
+            #endregion
+
+            return View(list2); //Views/Persons/Index.html
         }
     }
 }
