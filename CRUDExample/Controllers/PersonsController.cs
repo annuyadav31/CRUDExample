9fe30ce501a0db63902ef06c86c1cc91e28b697b
annuyadav31 <annuyadav99539@gmail.com>
2023-04-30 15:30:55 +0100

    Create Add Person View and Added logic for adding person

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -9,11 +9,13 @@ namespace CRUDExample.Controllers
     public class PersonsController : Controller
     {
         private readonly IPersonService _personsService;
+        private readonly ICountriesService _countriesService;
 
         //constructor
-        public PersonsController(IPersonService personsService)
+        public PersonsController(IPersonService personsService, ICountriesService countriesService)
         {
             _personsService = personsService;
+            _countriesService = countriesService;
         }
 
         [Route("persons/index")]
@@ -22,14 +24,15 @@ namespace CRUDExample.Controllers
         {
             //Search
             ViewBag.SearchFields = new Dictionary<string, string>()
-      {
-        { nameof(PersonResponse.PersonName), "Person Name" },
-        { nameof(PersonResponse.Email), "Email" },
-        { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
-        { nameof(PersonResponse.Gender), "Gender" },
-        { nameof(PersonResponse.CountryId), "Country" },
-        { nameof(PersonResponse.Address), "Address" }
-      };
+            {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                { nameof(PersonResponse.Email), "Email" },
+                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                { nameof(PersonResponse.Gender), "Gender" },
+                { nameof(PersonResponse.CountryId), "Country" },
+                { nameof(PersonResponse.Address), "Address" }
+            };
+
             List<PersonResponse> persons = _personsService.GetFilteredList(searchBy, searchString);
             ViewBag.CurrentSearchBy = searchBy;
             ViewBag.CurrentSearchString = searchString;
@@ -41,5 +44,36 @@ namespace CRUDExample.Controllers
 
             return View(sortedPersons); //Views/Persons/Index.cshtml
         }
+
+        [Route("persons/create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<CountryResponse> countries = _countriesService.GetCountryList();
+            ViewBag.Countries = countries;
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("persons/create")]
+        public IActionResult Create(PersonAddRequest personAddRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<CountryResponse> countries = _countriesService.GetCountryList();
+                ViewBag.Countries = countries;
+
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return View();
+            }
+
+            //call the service method
+            PersonResponse personResponse = _personsService.AddPerson(personAddRequest);
+
+            //navigate to Index() action method (it makes another get request to "persons/index"
+            return RedirectToAction("Index", "Persons");
+        }
+
     }
 }
