b0fb9dbfd4adb23d4971ede099e29404872850d3
annuyadav31 <annuyadav99539@gmail.com>
2023-04-30 18:02:59 +0100

    Added some sample data to seed in DbContext class

--- Entities/PersonsDbContext.cs
+++ Entities/PersonsDbContext.cs
@@ -11,6 +11,30 @@
 
             modelBuilder.Entity<Country>().ToTable("Countries");
             modelBuilder.Entity<Person>().ToTable("Persons");
+
+            //seed to countries
+            string countriesJson = System.IO.File.ReadAllText("countries.json");
+
+            List<Country>? countries =System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+            if (countries != null)
+            {
+                foreach (Country country in countries)
+                { modelBuilder.Entity<Country>().HasData(country); }
+            }
+
+            //seed to persons
+            //seed to countries
+            string personsJson = System.IO.File.ReadAllText("persons.json");
+
+            List<Person>? persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+            if (persons != null)
+            {
+                foreach (Person person in persons)
+                { modelBuilder.Entity<Person>().HasData(person); }
+            }
+
         }
     }
 }
