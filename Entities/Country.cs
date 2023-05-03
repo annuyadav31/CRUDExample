c15cd193a7fe87fb6e7fff15dc08f101aa4c9743
annuyadav31 <annuyadav99539@gmail.com>
2023-05-03 21:33:22 +0100

    getting list of all persons corresponding to one country

--- Entities/Country.cs
+++ Entities/Country.cs
@@ -8,5 +8,7 @@
         [Key]
         public Guid CountryId { get; set; } 
         public string? CountryName { get; set; }
+
+        //public virtual ICollection<Person>? Persons { get; set; }
     }
 }
